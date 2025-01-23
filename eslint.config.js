7109ddf8e2c2e3a196058b3c3f501c42f29e31c9
abusaleh123 <abusalehnoor4103@gmail.com>
2025-01-23 14:44:29 +0600

    Added Something

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,6 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [...compat.extends("next/core-web-vitals")];
-// module.exports = {
-//   parser: '@typescript-eslint/parser', // Use a valid parser name
-// };
-// module.exports = {
-//   reactStrictMode: true,
-// };
-// export default eslintConfig;
-
-
-const { dirname } = require("path");
-const { fileURLToPath } = require("url");
-const { FlatCompat } = require("@eslint/eslintrc");
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -30,17 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Getting the ESLint config
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-// Combine with next.js config
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  reactStrictMode: true,
-  eslintConfig,
+module.exports = {
+  parser: '@typescript-eslint/parser', // Use a valid parser name
 };
-
-module.exports = nextConfig;
-
+module.exports = {
+  reactStrictMode: true,
+};
+export default eslintConfig;
