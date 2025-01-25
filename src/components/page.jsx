31e97319bc8f83087f1e923680fb924b48b818a9
abusaleh123 <abusalehnoor4103@gmail.com>
2025-01-25 09:34:33 +0600

    Added Problem fix

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -9,7 +9,7 @@ const Blogs = ({data}) => {
             {
                 data.map(info => <div key={info.id} className="backdrop-blur-lg hover:bg-gray-800 bg-gray-950 h-36 flex flex-col justify-center items-center px-6 rounded-lg">
 
-                    <Link  href={`/blog/${info.id}`} className="text-lg hover:text-green-500 ">{info.title}</Link>
+                    <Link  href={`blog/${info.id}`} className="text-lg hover:text-green-500 ">{info.title}</Link>
                     <p className="h-8 w-8 flex justify-center items-center border-2 border-green-600 rounded-full absolute right-0 top-0">{info.id}</p>
                 </div>)
             }
